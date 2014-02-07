a0b1b6686e2aed06c75ea482516d1ad2611f7f1e
stymy <rschadmin@cmi-rsch-li002.childmind.org>
2014-02-07 16:33:57 -0500

    running c=100 e=0.01

--- svm_pipeline.py
+++ svm_pipeline.py
@@ -1,6 +1,7 @@
 import nipype.interfaces.afni as afni
 import nibabel as nb
 import numpy as np
+import os
 
 dataset = 'svr_test2'
 
@@ -11,50 +12,56 @@ total_mask = '/home/rschadmin/Data/'+dataset+'/automask+orig.BRIK'
 
 #find ROI range
 ROI_data = nb.load(ROIs).get_data()
-def train(input_data, ROI_data, total_mask):
+def train(input_data, ROI_data, total_mask, ROIs):
     #loop through each ROI
-    for ROI_num in np.unique(ROI_data):
+    for ROI_num in np.unique(ROI_data)[1:]:
         #set outputs
-        timeseries_data = '/home/rschadmin/Data/'+dataset+'/timeseries'+str(ROI_num)+'.1D'
-        mask = '/home/rschadmin/Data/'+dataset+'/mask'+str(ROI_num)+'.nii'
+        timeseries = '/home/rschadmin/Data/'+dataset+'/timeseries'+str(ROI_num)+'.1D'
+        mask = '/home/rschadmin/Data/'+dataset+'/mask'+str(ROI_num)+'+orig.BRIK'
         model = '/home/rschadmin/Data/'+dataset+'/model_run'+str(ROI_num)
-        vectors = '/home/rschadmin/Data/'+dataset+'/vectors_run'+str(ROI_num)
+        w = '/home/rschadmin/Data/'+dataset+'/w_run'+str(ROI_num)
         alphas = '/home/rschadmin/Data/'+dataset+'/alphas_run'+str(ROI_num)
 
         #make mask for not(ROI)
         #3dcalc -a '/home/rschadmin/Data/ROIs/craddock_2011_parcellations/rois200.nii' -expr 'not(equals(a,1))' -byte -prefix mask
+        #if not os.path.exists(mask):
+        dilation_str = '-c a+i -d a-i -e a+j -f a-j -g a+k -h a-k' 
+        #dilates ROI volume in three directions
+        
         masking = afni.Calc()
         masking.inputs.in_file_a = ROIs
         masking.inputs.in_file_b = total_mask
         ### exclude outer radius 1 voxel (dilate roi)
         masking.inputs.out_file = mask
-        masking.inputs.expr = 'and(b,not(equals(a,'+str(ROI_num)+')))'
-        masking.inputs.args = '-byte -overwrite'
+        masking.inputs.expr = 'and(b,not(amongst(1, equals(a,'+str(ROI_num)+'),c,d,e,f,g,h)))' #brain without dilated roi
+        masking.inputs.args = '-byte -overwrite '+dilation_str #byte required for 3dsvm
         maskRun = masking.run()
 
         #get timeseries (TSE Average ROI)
         #3dmaskave -quiet -mask ~/Data/ROIs/craddock_2011_parcellations/rois200.nii -mrange $i $i resampled_bandpassed_demeaned_filtered+tlrc.BRIK > timeseries$i.nii
         timing = afni.Maskave()
         timing.inputs.in_file = input_data
-        timing.inputs.out_file = timeseries_data
+        timing.inputs.out_file = timeseries
         timing.inputs.quiet = True
+        timing.inputs.mask = ROIs
         timing.inputs.args = '-overwrite -mrange '+str(ROI_num)+' '+str(ROI_num)
         timeRun = timing.run()
 
         #svm training
         training = afni.SVMTrain()
         training.inputs.in_file= input_data
-        training.inputs.out_file= vectors
-        training.inputs.trainlabels= timeRun.outputs.out_file
-        training.inputs.mask= maskRun.outputs.out_file
+        training.inputs.out_file= w
+        training.inputs.trainlabels= timeseries
+        training.inputs.mask= mask
         training.inputs.model= model
         training.inputs.alphas= alphas
         training.inputs.ttype = 'regression'
-        training.inputs.options = '-c 100 -e 0.001 -overwrite'
+        training.inputs.options = '-c 100 -e 0.01 -overwrite'
+        training.inputs.max_iterations = 100
         train_res = training.run()
 
 def test(ROI_data, input_data):
-    for ROI_num in np.unique(ROI_data):
+    for ROI_num in np.unique(ROI_data)[1:]:
         model = '/home/rschadmin/Data/svr_test/model_run'+str(ROI_num)+'+orig.BRIK'
         prediction = '/home/rschadmin/Data/svr_prediction/predict2with1_run'+str(ROI_num)
         testing = afni.SVMTest()
